7896f31bfbae5509528fc1d66b375b9a8dd6ff3c
m0rxn1 <chrege@163.com>
2022-11-10 17:22:48 +0800

    :bug: fix bug when use pow

--- alpine+socat+patchelf/deploy/run_pwn.sh
+++ alpine+socat+patchelf/deploy/run_pwn.sh
@@ -10,15 +10,16 @@ then
         echo "=================proof-of-work================="
         echo ""
         rand_str=$(head -c 27 /dev/urandom | base64)
-        hash_value=$(echo "$rand_str" | sha256sum - | cut -c 1-64)
-        backend=$(echo "$rand_str" | cut -c 4- )
+        hash_value=$(echo -n "$rand_str" | sha256sum - | cut -c 1-64)
+        frontend=$(echo "$rand_str" | cut -c -4 )
+        backend=$(echo "$rand_str" | cut -c 5- )
         prompt="sha256(XXXX + \"${backend}\") == ${hash_value}"
         echo $prompt
         echo -n "Gime me XXXX: "
 
         read -t 300 -r input_hash
 
-        if [ "$input_hash" != "$hash_value" ]
+        if [ "$input_hash" != "$frontend" ]
         then
             echo "Proof of work failed!"
             exit 2
